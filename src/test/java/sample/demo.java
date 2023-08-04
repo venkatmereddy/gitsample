764878bbf54ddcc75902f02fe32dfaac6e60d665
venkatmereddy <venkatmereddy@venkats-mbp>
2023-08-04 10:29:02 -0400

    commit

---
+++ src/test/java/sample/demo.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+
+	public void demo() {
+		
+		System.out.println("Hello this is my first commit");	
+
+	}
+
+}
